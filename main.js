dbb5f2f6f58789a4d4609c4a7bec50de07d8db79
Zain khokhar <135629202+zainkhokhar831@users.noreply.github.com>
2024-12-31 22:31:05 +0500

    second commit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function lastChar() {
-    let str = prompt("Enter a string to get its last character:");
+    let str = prompt("Enter a name to get its last character:");
     if (str.length > 0) {
         let lastCharacter = str.charAt(str.length - 1); 
-        alert("The last character of the string is: " + lastCharacter);
+        alert("The last character of the name is: " + lastCharacter);
     } else {
-        alert("Please enter a valid string.");
+        alert("Please enter a valid Name.");
     }
 }
 lastChar();
